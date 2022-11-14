3fa964e483eac75e321069e22af167cd8200dc09
aphly <121099327@qq.com>
2022-11-14 22:36:37 +0800

    update

--- src/Controllers/Admin/Controller.php
+++ src/Controllers/Admin/Controller.php
@@ -2,14 +2,13 @@
 
 namespace Aphly\LaravelPayment\Controllers\Admin;
 
-use Illuminate\Support\Facades\View;
+
 
 class Controller extends \Aphly\LaravelAdmin\Controllers\Controller
 {
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            //View::share("res",['title'=>'xxx']);
             return $next($request);
         });
         parent::__construct();
